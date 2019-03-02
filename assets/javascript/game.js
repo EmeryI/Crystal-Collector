c67e8b76258aeae99dab2e61dcb9b24532acb6ee
Emery I <ihegaranyau@gmail.com>
2019-03-02 13:48:53 -0500

    made variable for random number and set up jquery doc

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// tells the javascript to not load until the HTML has loaded first
+$(document).ready(function() {
+    console.log("javascript properly linked");
+
+    // this number indicates what the computer is randomly choosing
+    // the right side of the statement is used to make sure the lowest number is 19 and the highest number is 120 (101 + 19)
+    var random = Math.floor(Math.random() * 101) + 19;
+    console.log(random + " random value");
+
+
+
+});
